a4a5b096efd4addfc00956957ca7e2cdd2463fc9
Alumno <Alumno@LAB-BD-01>
2017-10-31 07:56:44 -0600

    Cambios de css

--- Monolith/src/java/FunciónMonolith/Ingreso.java
+++ Monolith/src/java/FunciónMonolith/Ingreso.java
@@ -24,17 +24,6 @@ public class Ingreso extends HttpServlet {
     private String NivelEstudios;
     
     
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    
-    
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
@@ -100,7 +89,177 @@ public class Ingreso extends HttpServlet {
         
         try {
             db.IngresarUsuario(u);
-            response.sendRedirect("web/");
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet Ingreso</title>");
+          out.println("<link href=\"Css/test1.css\" rel=\"stylesheet\" type=\"text/css\">\n" +
+"        <link href=\"Css/bootstraplogin.css\" rel=\"stylesheet\" type=\"text/css\">\n" +
+"        <link href=\"https://fonts.googleapis.com/css?family=Mukta\" rel=\"stylesheet\">\n" +
+"        <link rel=\"stylesheet\" href=\"Css/Menu.css\">");
+          out.println("<script type=\"text/javascript\">\n" +
+"            \n" +
+"\n" +
+"            // funcion para aumentar la fuente\n" +
+"            function agrandar() {\n" +
+"                var tamaño =  document.getElementById(\"uno\"); \n" +
+"                var tamaño2 = document.getElementById(\"dos\");\n" +
+"                var tamaño3 = document.getElementById(\"tres\");\n" +
+"                var tamaño4 = document.getElementById(\"cuatro\");\n" +
+"                var tamaño5 = document.getElementById(\"cinco\");\n" +
+"                var tamaño6 = document.getElementById(\"seis\");\n" +
+"                tamaño.style.height =  130 + \"%\";\n" +
+"                tamaño2.style.height = 100 + \"%\";\n" +
+"                tamaño3.style.height = 100 + \"%\";\n" +
+"                tamaño4.style.height = 100 + \"%\";\n" +
+"                tamaño5.style.height = 100 + \"%\";\n" +
+"                tamaño6.style.height = 100 + \"%\";\n" +
+"            }\n" +
+"            function agrandar2() {\n" +
+"                 var tamaño = document.getElementById(\"uno\");\n" +
+"                var tamaño2 = document.getElementById(\"dos\");\n" +
+"                var tamaño3 = document.getElementById(\"tres\");\n" +
+"                var tamaño4 = document.getElementById(\"cuatro\");\n" +
+"                var tamaño5 = document.getElementById(\"cinco\");\n" +
+"                var tamaño6 = document.getElementById(\"seis\");\n" +
+"                tamaño.style.height =  100 + \"%\";\n" +
+"                tamaño2.style.height = 130 + \"%\";\n" +
+"                tamaño3.style.height = 100 + \"%\";\n" +
+"                tamaño4.style.height = 100 + \"%\";\n" +
+"                tamaño5.style.height = 100 + \"%\";\n" +
+"                tamaño6.style.height = 100 + \"%\";\n" +
+"\n" +
+"            }\n" +
+"            function agrandar3() {\n" +
+"                var tamaño = document.getElementById(\"uno\");\n" +
+"                var tamaño2 = document.getElementById(\"dos\");\n" +
+"                var tamaño3 = document.getElementById(\"tres\");\n" +
+"                var tamaño4 = document.getElementById(\"cuatro\");\n" +
+"                var tamaño5 = document.getElementById(\"cinco\");\n" +
+"                var tamaño6 = document.getElementById(\"seis\");\n" +
+"                tamaño.style.height =  100 + \"%\";\n" +
+"                tamaño2.style.height = 100 + \"%\";\n" +
+"                tamaño3.style.height = 130 + \"%\";\n" +
+"                tamaño4.style.height = 100 + \"%\";\n" +
+"                tamaño5.style.height = 100 + \"%\";\n" +
+"                tamaño6.style.height = 100 + \"%\";\n" +
+"\n" +
+"            }\n" +
+"            function agrandar4() {\n" +
+"                 var tamaño = document.getElementById(\"uno\");\n" +
+"                var tamaño2 = document.getElementById(\"dos\");\n" +
+"                var tamaño3 = document.getElementById(\"tres\");\n" +
+"                var tamaño4 = document.getElementById(\"cuatro\");\n" +
+"                var tamaño5 = document.getElementById(\"cinco\");\n" +
+"                var tamaño6 = document.getElementById(\"seis\");\n" +
+"                tamaño.style.height =  100 + \"%\";\n" +
+"                tamaño2.style.height = 100 + \"%\";\n" +
+"                tamaño3.style.height = 100 + \"%\";\n" +
+"                tamaño4.style.height = 130 + \"%\";\n" +
+"                tamaño5.style.height = 100 + \"%\";\n" +
+"                tamaño6.style.height = 100 + \"%\";\n" +
+"\n" +
+"            }\n" +
+"            function agrandar5() {\n" +
+"                var tamaño = document.getElementById(\"uno\");\n" +
+"                var tamaño2 = document.getElementById(\"dos\");\n" +
+"                var tamaño3 = document.getElementById(\"tres\");\n" +
+"                var tamaño4 = document.getElementById(\"cuatro\");\n" +
+"                var tamaño5 = document.getElementById(\"cinco\");\n" +
+"                var tamaño6 = document.getElementById(\"seis\");\n" +
+"                tamaño.style.height =  100 + \"%\";\n" +
+"                tamaño2.style.height = 100 + \"%\";\n" +
+"                tamaño3.style.height = 100 + \"%\";\n" +
+"                tamaño4.style.height = 100 + \"%\";\n" +
+"                tamaño5.style.height = 130 + \"%\";\n" +
+"                tamaño6.style.height = 100 + \"%\";\n" +
+"\n" +
+"            }\n" +
+"            function agrandar6() {\n" +
+"                var tamaño = document.getElementById(\"uno\");\n" +
+"                var tamaño2 = document.getElementById(\"dos\");\n" +
+"                var tamaño3 = document.getElementById(\"tres\");\n" +
+"                var tamaño4 = document.getElementById(\"cuatro\");\n" +
+"                var tamaño5 = document.getElementById(\"cinco\");\n" +
+"                var tamaño6 = document.getElementById(\"seis\");\n" +
+"                tamaño.style.height =  100 + \"%\";\n" +
+"                tamaño2.style.height = 100 + \"%\";\n" +
+"                tamaño3.style.height = 100 + \"%\";\n" +
+"                tamaño4.style.height = 100 + \"%\";\n" +
+"                tamaño5.style.height = 100 + \"%\";\n" +
+"                tamaño6.style.height = 130 + \"%\";\n" +
+"\n" +
+"            }\n" +
+"\n" +
+"           \n" +
+"        </script>");
+
+            
+            out.println("</head>");
+            out.println("<body>");
+            out.println(" <div class=\"barraInicio navbar navbar-default \" style=\"font-family:Mukta\">Monolith<img  class=\"logo\" src=\"img/logo.png\" alt=\"\"></div>\n" +
+"        <header>\n" +
+"\n" +
+"            <div class=\"contenedor\" id=\"uno\" onclick=\"agrandar();\" ondblclick=\"minimizar();\">\n" +
+"                <img class=\"icon\" src=\"img/home.png\" alt=\"HomeLogo\">\n" +
+"            </div>\n" +
+"\n" +
+"            <div class=\"contenedor\" id=\"dos\" onclick=\"agrandar2();\" ondblclick=\"minimizar();\">\n" +
+"                <img class=\"icon\" src=\"img/examen.png\" alt=\"ExamenLogo\">\n" +
+"            </div>\n" +
+"\n" +
+"            <div class=\"contenedor\" id=\"tres\" onclick=\"agrandar3();\" ondblclick=\"minimizar();\">\n" +
+"                <img class=\"icon\" src=\"img/proyectos.png\" alt=\"ProyectosLogo\">\n" +
+"            </div>\n" +
+"\n" +
+"            <div class=\"contenedor\" id=\"cuatro\" onclick=\"agrandar4();\" ondblclick=\"minimizar();\">\n" +
+"                <img class=\"icon\" src=\"img/tareas.png\" alt=\"TareasLogo\">\n" +
+"            </div>\n" +
+"\n" +
+"            <div class=\"contenedor\" id=\"cinco\" onclick=\"agrandar5();\" ondblclick=\"minimizar();\">\n" +
+"                <img class=\"icon\" src=\"img/finanza.png\" alt=\"FinanzasLogo\">\n" +
+"            </div>\n" +
+"\n" +
+"            <div class=\"contenedor\" id=\"seis\" onclick=\"agrandar6();\" ondblclick=\"minimizar();\">\n" +
+"                <img class=\"icon\" src=\"img/ruta.png\" alt=\"RutaLogo\">\n" +
+"            </div>\n" +
+"\n" +
+"        </header>");
+            out.println("<div class='container' style='padding-top:200px;'>");
+            out.println("<div class='row' >");
+            out.println("<div class='col-12' >");
+            out.println("<h1 align='center'>Datos</h1>");
+            out.println("/<div>");
+            out.println("/<div>");
+            out.println("<div class='row' >");
+            out.println("<div class='col-12' >");
+            out.println("<h1 align='center'>Nombre Usuario :"+u.getNombre()+"</h1>");
+            out.println("/<div>");
+            out.println("/<div>");
+            out.println("<div class='row' >");
+            out.println("<div class='col-12' >");
+            out.println("<h1 align='center'>Nivel de Estudios :"+u.getNivelEstudios()+"</h1>");
+            out.println("/<div>");
+            out.println("/<div>");
+            out.println("<div class='row' >");
+            out.println("<div class='col-12' >");
+            out.println("<h1 align='center'>Direccion:"+u.getDireccion()+"</h1>");
+            out.println("/<div>");
+            out.println("/<div>");
+            out.println("<div class='row' >");
+            out.println("<div class='col-12' >");
+            out.println("<h1 align='center'>Pais :"+u.getPais()+"</h1>");
+            out.println("/<div>");
+            out.println("/<div>");
+            out.println("<div class='row' >");
+            out.println("<div class='col-12' >");
+            out.println("<h1 align='center'>Correo :"+u.getCorreo()+"</h1>");
+            out.println("/<div>");
+            out.println("/<div>");
+            out.println("</div>");
+            out.println("</body>");
+            out.println("</html>");
+            
         } catch (SQLException ex) {
                         out.print(ex.toString());
         }
